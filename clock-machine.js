323050aa35604894344982968213c35ba0a5c206
Pablo Alejandro Carravetti <pablo@pabli.net.ar>
2023-02-04 02:00:13 -0300

    testing php

---
+++ clock-machine.js
@@ -0,0 +1,17 @@
+const hour = document.querySelector('.hour'),
+minute = document.querySelector('.minute'),
+second = document.querySelector('.second'),
+screw = document.querySelector('.screw')
+
+const clockwise = () => {
+  const date = new Date()
+  let h = date.getHours() < 12 ? date.getHours() : date.getHours() - 12
+  let m = date.getMinutes()
+  let s = date.getSeconds()
+  let ms = date.getMilliseconds()
+  hour.style.transform = `rotate(${h * 30 + m / 2}deg)`
+  minute.style.transform = `rotate(${m * 6}deg)`
+  second.style.transform = `rotate(${(s * 6 + ms * 0.006) - 3}deg)`
+}
+
+setInterval(clockwise)
