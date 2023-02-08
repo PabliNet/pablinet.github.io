f491c1236e019fdf031afd6c84645065b93ae9d9
Pablo Alejandro Carravetti <pablo@pabli.net.ar>
2023-02-08 03:50:10 -0300

    reloj hsl

--- clock-machine.js
+++ clock-machine.js
@@ -8,10 +8,12 @@ const clockwise = () => {
   let h = date.getHours() < 12 ? date.getHours() : date.getHours() - 12
   let m = date.getMinutes()
   let s = date.getSeconds()
-  let ms = date.getMilliseconds()
   hour.style.transform = `rotate(${h * 30 + m / 2}deg)`
+  document.documentElement.style.setProperty('--clock-hour-fill', h * 30 + m / 2)
   minute.style.transform = `rotate(${m * 6}deg)`
+  document.documentElement.style.setProperty('--clock-minute-fill', m * 6)
   second.style.transform = `rotate(${s * 6}deg)`
+  document.documentElement.style.setProperty('--clock-second-fill', s * 6)
 }
 
 setInterval(clockwise)
